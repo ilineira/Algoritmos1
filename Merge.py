4d330ef692a65e46076fea3d061cbc9eb6ab53bb
ilineira <ilineira@github.com>
2018-07-05 13:51:10 -0300

    commit

---
+++ Merge.py
@@ -0,0 +1,77 @@
+def leer(archivo):
+    linea = archivo.readline()
+    if linea:
+        return linea.strip('\n')
+    else:
+        return
+
+def agregar_a(nuevo, palabra):
+    print(palabra)
+    nuevo.write(palabra + '\n')
+
+def merge(txt1, txt2, txt3, nuevo, max):
+    palabra1 = leer(txt1)
+    palabra2 = leer(txt2)
+    palabra3 = leer(txt3)
+    l = [palabra1, palabra2, palabra3]
+    lista = []
+    while (palabra1 != max) or (palabra2 != max) or (palabra3 != max):
+        l.sort()
+        print(l)
+
+        if l[0] == max:
+            break
+
+        elif l[0] == l[1] == l[2]:
+            agregar_a(nuevo, palabra1)
+            aux = palabra1
+            aux2 = palabra2
+            aux3 = palabra3
+            while aux == palabra1:
+                palabra1 = leer(txt1)
+            while aux2 == palabra2:
+                palabra2 = leer(txt2)
+            while aux3 == palabra3:
+                palabra3 = leer(txt3)
+            l = [palabra1, palabra2, palabra3]
+
+        elif l[0] == l[1]:
+            agregar_a(nuevo, palabra1)
+            aux = palabra1
+            aux2 = palabra2
+            while aux == palabra1:
+                palabra1 = leer(txt1)
+            while aux2 == palabra2:
+                palabra2 = leer(txt2)
+            l = [palabra1, palabra2, palabra3]
+
+        elif l[0] == palabra1:
+            agregar_a(nuevo, palabra2)
+            aux = palabra1
+            while aux == palabra1:
+                palabra1 = leer(txt1)
+            l = [palabra1, palabra2, palabra3]
+
+        elif l[0] == palabra2:
+            agregar_a(nuevo, palabra2)
+            aux = palabra2
+            while aux == palabra2:
+                palabra2 = leer(txt2)
+            l = [palabra1, palabra2, palabra3]
+
+        else:
+            agregar_a(nuevo, palabra3)
+            aux = palabra3
+            while aux == palabra3:
+                palabra3 = leer(txt3)
+            l = [palabra1, palabra2, palabra3]
+
+max = 'zzz'
+t1 = open('archivo1.txt', 'r')
+t2 = open('archivo2.txt', 'r')
+t3 = open('archivo3.txt', 'r')
+n = open('palabras.txt', 'w')
+merge(t1, t2, t3, n)
+t1.close()
+t2.close()
+t3.close()
