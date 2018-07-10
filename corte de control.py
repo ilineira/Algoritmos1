0e9de4286bb70e1ea3a4990c597aa76d9e325f32
ilineira <ilineira@github.com>
2018-07-10 13:15:36 -0300

    commit

--- corte de control.py
+++ corte de control.py
@@ -1,56 +1,25 @@
-# ventas.py: Recorre un archivo de ventas e imprime totales y subtotales
+def leer_info(ejs, default):
+    linea = ejs.readline()
+    return linea if linea else default
 
-# encoding: latin1
-import csv
-
-def leer_datos(datos):
-    """ Devuelve el siguiente registro o None si no hay más """
-    try:
-        return datos.next()
-    except:
-        return None
-
-def ventas_clientes_mes(archivo_ventas):
-    """ Recorre un archivo csv, con la información almacenada en el
-        formato: cliente,an"o,mes,día,venta """
-    # Inicialización
-    ventas = open(archivo_ventas)
-    ventas_csv = csv.reader(ventas)
-
-    item = leer_datos(ventas_csv)
+with open('movimientos.txt', 'rt') as movs:
+    linea = leer_info(movs, '999999,0')
+    nro_cta, importe = linea.rstrip().split(',')
+    max = '999999'
+    print (nro_cta, importe)
     total = 0
-
-    while item:
-        # Inicialización para el bucle de cliente
-        cliente = item[0]
-        total_cliente = 0
-        print ("Cliente %s" % cliente)
-
-        while item and item[0] == cliente:
-            # Inicialización para el bucle de año
-            anyo = item[1]
-            total_anyo = 0
-            print ("\tAño: %s" % anyo)
-
-            while item and item[0] == cliente and item[1] == anyo:
-                mes, monto = item[2], float(item[3])
-                print ("\t\tVentas del mes %s: %.2f" % (mes, monto))
-                total_anyo += monto
-                # Siguiente registro
-                item = leer_datos(ventas_csv)
-
-        # Final del bucle de año
-        print ("\tTotal para el año %s: %.2f" % (anyo, total_anyo))
-        total_cliente += total_anyo
-
-    # Final del bucle de cliente
-    print ("Total para el cliente %s: %.2f\n" % (cliente, total_cliente))
-    total += total_cliente
-
-# Final del bucle principal
-print ("Total general: %.2f" % total)
-
-# Cierre del archivo
-ventas.close()
-
-ventas_clientes_mes("ventas.csv")
+    while nro_cta != max:
+        cta_ant = nro_cta
+        tot_cta = 0
+        print ('cta',cta_ant)
+        print ('importe',importe)
+        while nro_cta == cta_ant and nro_cta != max:
+            print (importe)
+            tot_cta+=int(importe)
+            print (nro_cta, importe)
+            linea = leer_info(movs, '999999,0')
+            nro_cta, importe = linea.rstrip().split(',')
+        print ('cierre')
+        print ('El total de la cuenta {} es {}:'.format(cta_ant, tot_cta))
+        total += tot_cta
+print ('Total Gral:', total)
