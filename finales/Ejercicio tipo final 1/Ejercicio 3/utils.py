fd9ed4d037deda036bc4a22bf7864999a20f03e4
ilineira <ilineira@github.com>
2018-07-15 16:16:48 -0300

    commit

---
+++ finales/Ejercicio tipo final 1/Ejercicio 3/utils.py
@@ -0,0 +1,9 @@
+def guardar_en_archivo(archivo, contenido):
+    return
+
+
+def leer_desde_archivo(archivo):
+    linea = archivo.readline()
+    padron, nombre, apellido, nota, codigo_departamento, codigo_materia = linea.strip('\n').split(',')
+    data = Data()
+    return data, fin_de_archivo
